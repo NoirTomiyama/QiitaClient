78b9e73829e6f7653535a907ee63be45d4718ecb
NoirTomiyama <lit.karu.noir@gmail.com>
2019-11-11 13:03:02 +0900

    create QiitaClient

---
+++ app/src/main/java/com/noir/qiitaclient/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.noir.qiitaclient
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
