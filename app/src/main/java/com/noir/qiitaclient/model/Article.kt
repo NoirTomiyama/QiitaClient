1177ffecf156df893e44bb43a7d56d89d30ab326
NoirTomiyama <lit.karu.noir@gmail.com>
2019-11-11 13:06:58 +0900

    対象データの定義

---
+++ app/src/main/java/com/noir/qiitaclient/model/Article.kt
@@ -0,0 +1,3 @@
+package com.noir.qiitaclient.model
+
+data class Article(val id: String, val title: String, val url: String, val user: User)
---
+++ app/src/main/java/com/noir/qiitaclient/model/User.kt
@@ -0,0 +1,3 @@
+package com.noir.qiitaclient.model
+
+data class User(val id: String, val name: String, val profileImageUrl: String)
